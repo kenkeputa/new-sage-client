9fc303627bee553a0d56c691559b5900635674be
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-12 13:19:51 +0100

    fuk

--- src/loan/application/allactivities.jsx
+++ src/loan/application/allactivities.jsx
@@ -239,7 +239,7 @@ let Activity = ({datatable})=>{
             >
               <div className="grow shrink basis-0 h-[30px] justify-start items-center gap-3 flex">
                 <div className="grow shrink basis-0 text-[#565656] text-sm font-medium font-['Mulish'] leading-[21px]">
-                {new Date(row.created_at).toISOString().split('T')[0].split('-').reverse().join('/')}
+                {new Date(row?.created_at).toISOString().split('T')[0].split('-').reverse().join('/')}
 
 
                 </div>
