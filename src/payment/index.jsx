413b08d844a63bb135abe938f2bc081c3e024276
uhunomapaul-codinplus31 <paul.uhunoma@kenkeputa.com>
2025-03-16 00:32:10 +0100

    Update index.jsx

--- src/payment/index.jsx
+++ src/payment/index.jsx
@@ -115,9 +115,9 @@ function Payment() {
         }
         )}
     </div>
-    {index === 0 && main.length === 2 ?
-    <RegularActivity datatable={main[0]?.[0].record}/> : index === 1 && main.length === 2 ? <LoanActivity  datatable={main[1]?.[0].record}/> :
-   index === 3 && main.length === 2 ? <RegularActivity datatable={main[0]?.[0].record}/> : ""}
+    {index === 0 && main[0]?.[0].record ?
+    <RegularActivity datatable={main[0]?.[0].record}/> : index === 1 && main[1]?.[0].record ? <LoanActivity  datatable={main[1]?.[0].record}/> :
+   index === 3 && main[0]?.[0].record ? <RegularActivity datatable={main[0]?.[0].record}/> : ""}
     
     </div>);
 }
